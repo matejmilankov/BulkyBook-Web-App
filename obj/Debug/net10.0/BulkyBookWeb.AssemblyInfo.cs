c4a7a0285bff3241f690f9f0681cd7f13387045c
Matej Milankov <matej.milankov@gmail.com>
2026-03-11 16:14:52 +0100

    Configuration for data base

--- obj/Debug/net10.0/BulkyBookWeb.AssemblyInfo.cs
+++ obj/Debug/net10.0/BulkyBookWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3906e03cd917d7c79ab389e54fc21650e1fdbc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
